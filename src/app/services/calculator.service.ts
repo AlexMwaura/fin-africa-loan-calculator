d71092ea86f78ae7c80bfa1d885f1581f57bc799
Alex Mwaura <alexmwaura36@gmail.com>
2025-07-19 22:34:49 +0300

    update the api url

--- src/app/services/calculator.service.ts
+++ src/app/services/calculator.service.ts
@@ -1,23 +1,41 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 export interface LoanCalculationRequest {
   grossIncome: number;
   totalDeductions: number;
 }
-
+export interface LoanCalculationResponse {
+  netIncome: number;
+  maxLoanAmount: number;
+  eligible: boolean;
+  message: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CalculatorService {
 
-    private apiUrl = 'http://localhost:8080/api/calculate'; 
+    private apiUrl = 'http://localhost:8089/api/v1/loan-calculator/calculate'; 
 
   constructor(private http: HttpClient) { }
 
-  calculateAffordability(income: number, deductions: number): Observable<any> {
-    return this.http.post<any>(this.apiUrl, { income, deductions });
+calculateAffordability(
+    grossIncome: number,
+    totalDeductions: number,
+    token?: string 
+  ): Observable<LoanCalculationResponse> {
+    const request: LoanCalculationRequest = {
+      grossIncome,
+      totalDeductions
+    };
+
+    const headers = token
+      ? new HttpHeaders({ Authorization: `Bearer ${token}` })
+      : undefined;
+
+    return this.http.post<LoanCalculationResponse>(this.apiUrl, request, { headers });
   }
 }
